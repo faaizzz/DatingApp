52b84f01a6b35d1d7cd6ab5c4835dd85e5d1f78c
Faaiz K Rissad <faaiz.rissad@wipro.com>
2021-05-09 14:20:55 +0530

    Configured Automapper for photos

--- API/Data/UserRepository .cs
+++ API/Data/UserRepository .cs
@@ -22,12 +22,14 @@ namespace API.Data
         public async Task<AppUser> GetUserByUsernameAsync(string username)
         {
            return await _context.Users
+                .Include(p=>p.Photos)
                 .SingleOrDefaultAsync(x => x.UserName == username);
         }
 
         public async Task<IEnumerable<AppUser>> GetUsersAsync()
         {
             return await _context.Users
+                .Include(p=>p.Photos)
                 .ToListAsync();
         }
 
