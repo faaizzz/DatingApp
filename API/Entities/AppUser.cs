12d8e42dab6dca6aed2bc54816c74b82a43e1257
Faaiz K Rissad <faaiz.rissad@wipro.com>
2021-05-09 08:03:07 +0530

    Calculated Date Of Birth

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using API.Extentions;
 
 namespace API.Entities
 {
@@ -21,6 +22,10 @@ namespace API.Entities
         public string City { get; set; }
         public string Country { get; set; }
         public ICollection<Photo> Photos { get; set; }
+        public int GetAge()
+        {
+            return DateOfBirth.CalculateAge();
+        }
 
 
     }
