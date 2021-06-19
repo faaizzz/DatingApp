63f6119304cc930f5a376a40503c811663783c79
Faaiz K Rissad <faaiz.rissad@wipro.com>
2021-06-19 15:17:18 +0530

    Added Testing Project and created unit test for WeatherForecast controller

---
+++ DatingApp.Tests/Controller/WeatherForecastControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using API;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DatingApp.Tests.Controller
+{
+    public class WeatherForecastControllerTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly HttpClient _client;
+
+        public WeatherForecastControllerTests(WebApplicationFactory<Startup> factory)
+        {
+            factory.ClientOptions.BaseAddress = new Uri("https://localhost:5001/WeatherForecast");
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task GetAll_ReturnsExpectedResponse()
+        {
+            var response = await _client.GetAsync("");
+
+            var result  = await _client.GetFromJsonAsync<WeatherForecast[]>("");
+        }
+    }
+}
